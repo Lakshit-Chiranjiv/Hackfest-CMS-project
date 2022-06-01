aea7f422714eed5ebdf198872faafa1378e0c235
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-01 22:08:01 +0530

    added mongoose and imported uri

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-
-
+import mongoose from "mongoose";
+import { MONGO_DB_URI } from './dbConfig.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const PORT = 5000;
 
 app.get('/',(req,res)=>{
     res.json({msg: 'cms server'});
@@ -21,4 +23,4 @@ app.get('/',(req,res)=>{
 //endpoint to return all the data in a schema
 
 
-app.listen(5000,()=> console.log("CMS server listening on port 5000"));
+app.listen(PORT,()=> console.log("CMS server listening on port 5000"));
