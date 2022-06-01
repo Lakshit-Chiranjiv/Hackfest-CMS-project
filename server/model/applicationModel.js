f88be84f6fcd7bb057c4cb7ab13a8991ff893fff
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-01 22:29:57 +0530

    added application model

---
+++ server/model/applicationModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const applicationSchema = mongoose.Schema({
+    number: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    lastUpdated: {
+        type: Date,
+        required: true
+    },
+    banner: {
+        type: Image,
+        required: false
+    }
+});
+
+const applicationModel = mongoose.model('applications',applicationSchema);
+
+export default applicationModel;
