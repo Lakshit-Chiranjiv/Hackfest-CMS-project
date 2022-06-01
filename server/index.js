3eb3d656da95cd5e75a8007210c39b0549de71f8
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-01 21:58:42 +0530

    changed to type module and added some place comments

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+
+
 
 const app = express();
 
@@ -8,5 +10,14 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.json({msg: 'cms server'});
 })
+
+//endpoint which returns all applications
+
+//endpoint which returns all schemas in an application
+
+//endpoint which returns all input field names and types in a schema
+
+//endpoint to return all the data in a schema
+
 
 app.listen(5000,()=> console.log("CMS server listening on port 5000"));
