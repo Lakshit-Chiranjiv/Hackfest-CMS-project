a9a0d0c305d5240fe995a6f53c4623254c9d1767
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-02 21:17:47 +0530

    added the custom schema model

---
+++ server/model/customSchemaModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const customSchemaSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    inputFields: {
+        type: [mongoose.Types.ObjectId],
+        required: false
+    }
+});
+
+const customSchemaModel = mongoose.model('customSchemas',customSchemaSchema);
+
+export default customSchemaModel;
