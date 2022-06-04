cefa6acc090cdaca62c764fd2d61061d2c4c84e5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-04 19:21:23 +0530

    func to get one schemas of a project

---
+++ server/controllers/getOneSchema.js
@@ -0,0 +1,15 @@
+import dataModel from "../models/dataModel.js";
+import { returnUserData } from "./returnUserData.js";
+
+export const getAllSchemas = async(req,res) => {
+    const { email,pid,sid } = req.params;
+    try {
+        const foundUser = returnUserData(email);
+        const selectedProject = foundUser.projects.find(project => project.projectId===pid)
+        const selectedSchema = selectedProject.schemas.find(schema => schema.schemaId===sid);
+        if(!selectedSchema) throw Error('cannot get due to some server error');
+        res.status(200).json(selectedSchema);
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
