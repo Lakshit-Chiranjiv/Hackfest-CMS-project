c7c6dbd0a249dcc8a06ac5bad96d3c4b4aeaa9cf
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-04 19:03:26 +0530

    func to get all schemas of a project

---
+++ server/controllers/getAllSchemas.js
@@ -0,0 +1,15 @@
+import dataModel from "../models/dataModel.js";
+import { returnUserData } from "./returnUserData.js";
+
+export const getAllSchemas = async(req,res) => {
+    const { email,pid } = req.params;
+    try {
+        const foundUser = returnUserData(email);
+        const selectedProject = foundUser.projects.find(project => project.projectId===pid)
+        const allSchemas = selectedProject.schemas;
+        if(!allSchemas) throw Error('cannot get due to some server error');
+        res.status(200).json(allSchemas);
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
