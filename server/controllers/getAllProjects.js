12217bf3965a784e62dd8623594bd8813bf0073f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-04 18:50:14 +0530

    corrected get all projects route

--- server/controllers/getAllProjects.js
+++ server/controllers/getAllProjects.js
@@ -1,8 +1,11 @@
-import projectModel from "../models/projectModel.js";
+import dataModel from "../models/dataModel.js";
+import { returnUserData } from "./returnUserData.js";
 
 export const getAllProjects = async(req,res) => {
+    const { email } = req.params;
     try {
-        const allProjects = await projectModel.find();
+        const foundUser = returnUserData(email);
+        const allProjects = foundUser.projects;
         if(!allProjects) throw Error('cannot get due to some server error');
         res.status(200).json(allProjects);
     } catch (error) {
