c5b2e90c1dfc8a04cd08220447983ebc15bff4e4
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-04 21:23:14 +0530

    func to add project

---
+++ server/controllers/addProject.js
@@ -0,0 +1,26 @@
+import dataModel from "../models/dataModel.js";
+import { returnUserData } from "./returnUserData.js";
+
+export const addProject = async(req,res) => {
+    const { id } = req.params;
+    let allProjects;
+
+    try {
+        const foundUser = returnUserData(email);
+        allProjects = foundUser.projects;
+        if(!allProjects) throw Error('cannot get due to some server error');
+        res.status(200).json(allProjects);
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+
+    const newProjectList = [...allProjects,req.body]
+
+    try {
+        const updatedUser = await dataModel.findByIdAndUpdate(id,newProjectList);
+        if(!updatedUser) throw Error('could not add project');
+        res.status(200).json({message: `successfully added project : ${req.body.name}`});
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
