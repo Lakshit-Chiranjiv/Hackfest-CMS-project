27fdbfc77e5a674b7ff7db076bd4ba8587669569
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-04 18:40:14 +0530

    created func for getting user data on server

--- server/controllers/getUserData.js
+++ server/controllers/getUserData.js
@@ -1,9 +1,10 @@
 import dataModel from "../models/dataModel.js";
+import { returnUserData } from "./returnUserData.js";
 
 export const getUserData = async(req,res) => {
     const { email } = req.params;
     try {
-        const foundUser = await dataModel.find({ email });
+        const foundUser = returnUserData(email);
         if(!foundUser) throw Error('could not find user');
         res.status(200).json(foundUser);
     } catch (error) {
