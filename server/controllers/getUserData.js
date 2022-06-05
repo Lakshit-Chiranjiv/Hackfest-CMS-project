4dd4cb14eb218385343269c83fc9354ef8a99dc3
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 15:18:09 +0530

    .

--- server/controllers/getUserData.js
+++ server/controllers/getUserData.js
@@ -3,7 +3,7 @@ import { returnUserData } from "./returnUserData.js";
 export const getUserData = async(req,res) => {
     const { email } = req.params;
     try {
-        const foundUser = returnUserData(email);
+        const foundUser = await returnUserData(email);
         if(!foundUser) throw Error('could not find user');
         res.status(200).json(foundUser);
     } catch (error) {
