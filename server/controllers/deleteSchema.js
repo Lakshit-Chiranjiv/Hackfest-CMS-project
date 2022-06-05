395ba7123184fc9a9ffc8775aa079c5ec3bad15d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 08:40:51 +0530

    func to delete a schema from a project

---
+++ server/controllers/deleteSchema.js
@@ -0,0 +1,28 @@
+import dataModel from "../models/dataModel.js";
+import { returnUserById } from './returnUserById.js'
+
+export const deleteSchema = async(req,res) => {
+    const { id,pid,sid } = req.params;
+    let foundUser;
+    let allSchemas;
+    try {
+        foundUser = returnUserById(id);
+        const selectedProject = foundUser.projects.find(project => project.projectId===pid)
+        allSchemas = selectedProject.schemas;
+        if(!allSchemas) throw Error('cannot get user schemas due to some server error');
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+
+    const filteredSchemas = allSchemas.filter(schema => schema.schemaId!==sid)
+
+    foundUser.projects.find(project => project.projectId===pid).schemas = filteredSchemas;
+
+    try {
+        const updatedUser = await dataModel.findByIdAndUpdate(id,foundUser);
+        if(!updatedUser) throw Error('could not delete schema');
+        res.status(200).json({message: `successfully deleted schema`});
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
