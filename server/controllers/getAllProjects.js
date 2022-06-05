3b09d855cf15ab4795115352c4ccaf5ab8451b90
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 15:17:34 +0530

    minor correction

--- server/controllers/getAllProjects.js
+++ server/controllers/getAllProjects.js
@@ -3,11 +3,12 @@ import { returnUserData } from "./returnUserData.js";
 export const getAllProjects = async(req,res) => {
     const { email } = req.params;
     try {
-        const foundUser = returnUserData(email);
+        const foundUsers = await returnUserData(email);
+        const foundUser = foundUsers[0];
         const allProjects = foundUser.projects;
         if(!allProjects) throw Error('cannot get due to some server error');
         res.status(200).json(allProjects);
     } catch (error) {
-        res.status(400).json({message: error});
+        res.status(400).json({message: error.message});
     }
 }
