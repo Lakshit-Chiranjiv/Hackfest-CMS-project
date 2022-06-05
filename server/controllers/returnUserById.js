56fb28545f4ff43ce2436131b3d06dc4fe1cb29b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 08:26:15 +0530

    corrected func name

--- server/controllers/returnUserById.js
+++ server/controllers/returnUserById.js
@@ -1,6 +1,6 @@
 import dataModel from "../models/dataModel.js";
 
-export const returnUserData = async(id) => {
+export const returnUserById = async(id) => {
     try {
         const foundUser = await dataModel.find({ _id: id });
         if(!foundUser) throw Error('could not find user');
