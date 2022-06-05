98089450680639b4df0e1a6ebfe5b1a81596bd69
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 08:23:13 +0530

    func to add new schema

---
+++ server/controllers/addSchema.js
@@ -0,0 +1,28 @@
+import dataModel from "../models/dataModel.js";
+import { returnUserData } from "./returnUserData.js";
+
+export const addSchema = async(req,res) => {
+    const { id,pid } = req.params;
+    let allSchema;
+    let foundUser;
+
+    try {
+        foundUser = returnUserData(email);
+        const selectedProject = foundUser.projects.find(project => project.projectId===pid)
+        allSchemas = selectedProject.schemas;
+        if(!allSchemas) throw Error('cannot get due to some server error');
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+
+
+    foundUser.projects.find(project => project.projectId===pid).schemas.push(req.body);
+
+    try {
+        const updatedUser = await dataModel.findByIdAndUpdate(id,foundUser);
+        if(!updatedUser) throw Error('could not add schema');
+        res.status(200).json({message: `successfully added schema : ${req.body.name}`});
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
