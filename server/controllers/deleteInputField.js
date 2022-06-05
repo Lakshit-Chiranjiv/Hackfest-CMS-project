1d76ebbd965f1308dc56123e85298a0c1df37def
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 08:54:05 +0530

    func to delete Input field

---
+++ server/controllers/deleteInputField.js
@@ -0,0 +1,29 @@
+import { returnUserById } from './returnUserById.js'
+import dataModel from "../models/dataModel.js";
+
+export const deleteInputField = async(req,res) => {
+    const { id,pid,sid,fid } = req.params;
+    let foundUser;
+    let schemaInputFields;
+    try {
+        foundUser = returnUserById(id);
+        const selectedProject = foundUser.projects.find(project => project.projectId===pid)
+        const selectedSchema = selectedProject.schemas.find(schema => schema.schemaId===sid)
+        schemaInputFields = selectedSchema.inputFields;
+        if(!schemaInputFields) throw Error('cannot get input fields due to some server error');
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+
+    const filteredschemaInputFields = schemaInputFields.filter(field => field.fieldId!==fid);
+
+    foundUser.projects.find(project => project.projectId===pid).schemas.find(schema => schema.schemaId===sid).inputFields = filteredschemaInputFields;
+
+    try {
+        const updatedUser = await dataModel.findByIdAndUpdate(id,foundUser);
+        if(!updatedUser) throw Error('could not delete input field');
+        res.status(200).json({message: `successfully deleted input field`});
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
