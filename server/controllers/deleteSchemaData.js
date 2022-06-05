17158167ec469b967240cedf99ee97dd2047b3fc
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 13:23:33 +0530

    func to delete schema data

---
+++ server/controllers/deleteSchemaData.js
@@ -0,0 +1,29 @@
+import { returnUserById } from "./returnUserById.js";
+
+export const deleteSchemaData = async(req,res) => {
+    const { id,pid,sid,did } = req.params;
+
+    let foundUser;
+    let schemaData;
+    try {
+        foundUser = returnUserById(id);
+        const selectedProject = foundUser.projects.find(project => project.projectId===pid)
+        const selectedSchema = selectedProject.schemas.find(schema => schema.schemaId===sid);
+        schemaData = selectedSchema.data;
+        if(!schemaData) throw Error('cannot get due to some server error');
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+
+    const filteredSchemaData = schemaData.filter(data => data.dataId!==did);
+
+    foundUser.projects.find(project => project.projectId===pid).schemas.find(schema => schema.schemaId===sid).data = filteredSchemaData;
+
+    try {
+        const updatedUser = await dataModel.findByIdAndUpdate(id,foundUser);
+        if(!updatedUser) throw Error('could not delete data');
+        res.status(200).json({message: `successfully deleted data`});
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
