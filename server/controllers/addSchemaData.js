aa59b82b0b5d4c95c67297aa024c5c0983420a27
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-05 13:23:45 +0530

    func to add schema data

---
+++ server/controllers/addSchemaData.js
@@ -0,0 +1,23 @@
+import { returnUserById } from "./returnUserById.js";
+
+export const deleteSchemaData = async(req,res) => {
+    const { id,pid,sid } = req.params;
+
+    let foundUser;
+    try {
+        foundUser = returnUserById(id);
+        if(!foundUser) throw Error('cannot get user due to some server error');
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+
+    foundUser.projects.find(project => project.projectId===pid).schemas.find(schema => schema.schemaId===sid).data.push(req.body);
+
+    try {
+        const updatedUser = await dataModel.findByIdAndUpdate(id,foundUser);
+        if(!updatedUser) throw Error('could not add data');
+        res.status(200).json({message: `successfully created data`});
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
