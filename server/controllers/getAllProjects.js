90dcee4bd6dbeca4c0ea354f63ab7b43ca95192f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-06 07:56:35 +0530

    called the loadendpoints func

--- server/controllers/getAllProjects.js
+++ server/controllers/getAllProjects.js
@@ -1,7 +1,10 @@
+import { loadAllEndpoints } from "../index.js";
 import { returnUserData } from "./returnUserData.js";
 
 export const getAllProjects = async(req,res) => {
     const { email } = req.params;
+    loadAllEndpoints(email);
+    
     try {
         const foundUsers = await returnUserData(email);
         const foundUser = foundUsers[0];
